65f2b25f95674a9de48ca2823a9e1927462e5241
rizqa febryan prastyo <47109047+cicero78M@users.noreply.github.com>
2025-06-20 11:03:15 +0700

    Add class management model and endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
 const questionRoutes = require('./questionRoutes');
 const questionBankRoutes = require('./questionBankRoutes');
+const classRoutes = require('./classRoutes');
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/questions', questionRoutes);
 router.use('/question-banks', questionBankRoutes);
+router.use('/classes', classRoutes);
 
 module.exports = router;
